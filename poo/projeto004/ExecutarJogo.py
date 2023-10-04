3f3eb6e7f81c0f8de42ed6f8a1d8514a22189ba1
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2023-10-04 19:39:40 -0300

    Update ExecutarJogo.py

--- poo/projeto004/ExecutarJogo.py
+++ poo/projeto004/ExecutarJogo.py
@@ -1,2 +1,13 @@
-import JogandoDados
+import JogandoDados as Dado
 import Lutador
+
+""" Aqui inicia o programa """
+#Exemplo 01
+jogar = Dado.JogandoDados()
+jogar.set_numeroDeLados(10)
+#print(jogar.get_numeroDeLados())
+#print(jogar.jogarDado(), end=" ")
+Mago = Lutador.Lutador("Merlin", 155, 159, 100, 20, 10, jogar)
+print(" Lutador: {0}".format(Mago))
+print(" Saude: {0}".format( Mago.barraDeSaude() ))
+print(" Esta vivo: {0}".format( Mago.estaVivo() ))
