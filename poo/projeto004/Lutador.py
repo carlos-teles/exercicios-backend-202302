fd52ce29a5915bec99c724866accc338e9905a85
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2023-10-05 18:21:27 -0300

    Update Lutador.py

--- poo/projeto004/Lutador.py
+++ poo/projeto004/Lutador.py
@@ -13,7 +13,7 @@ class Lutador:
         self.__valorDoDado = valorDoDado
         self.__mensagem = ""
     def __str__ (self):
-        return str(self.__nome),str(self.__saude)
+        return str(self.__nome)
     def estaVivo( self ):
         return self.__saude > 0
     def barraDeSaude ( self ):
@@ -22,3 +22,12 @@ class Lutador:
         if ( contador == 0 and self.estaVivo() ):
             contador = 1
         return "[{0}{1}]".format("#" * contador, " "*(total - contador))
+    def defesa( self, dano ):
+        golpe = dano - ( self.__defesa + self.__valorDoDado.jogarDado() )
+        if golpe > 0:
+            self.__saude = self.__saude - golpe
+            if self.__saude < 0:
+                self.__saude = 0
+    def ataque( self, inimigo ):
+        hit = self.__dano  + self.__valorDoDado.jogarDado()
+        inimigo.defesa( hit )
