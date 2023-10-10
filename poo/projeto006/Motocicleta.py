3cd3cf3cd7f8f6f2996296bf7bf7858fb0471d4b
Carlos Teles <carlos_alberto_teles@hotmail.com>
2023-10-10 22:40:47 +0000

    Novo projeto

---
+++ poo/projeto006/Motocicleta.py
@@ -0,0 +1,12 @@
+import Veiculo
+class Motocicleta( Veiculo.Veiculo ):
+    def __init__(self, chassi, marca, modelo, ano, cilindrada, numero_rodas ):
+        super().__init__(chassi, marca, modelo, ano)
+        super().set_tipo("Motocicleta")
+        self.cilindrada = cilindrada
+        self.numero_rodas = numero_rodas
+""" Aqui comeca o teste """
+Moto = Motocicleta('5AZKG01Z12A339037', 'Honda', 'CG', '2015', 1200, 2)
+print(Moto.get_tipo())
+print(Moto.numero_rodas)
+print(Moto.marca)
