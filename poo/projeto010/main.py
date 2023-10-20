139f53af65fa6e5d13c32f883a546379af5620e2
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2023-10-20 20:00:13 -0300

    "Inicio"

---
+++ poo/projeto010/main.py
@@ -0,0 +1,11 @@
+import fastapi
+
+app = fastapi.FastAPI()
+
+@app.get("/")
+def indice():
+    return{"mensagem" : "Ola Mundo."}
+
+@app.get("/mundo")
+def indiceMundo():
+    return{"mensagem" : "Ola Todo Mundo."}
