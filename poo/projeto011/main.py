645a39415c7e51b1adf6bcb8fe1625c6bd9d9a88
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2023-10-24 22:10:50 +0000

    Tue, Oct 24, 2023, 7:10 PM -03:00

--- poo/projeto011/main.py
+++ poo/projeto011/main.py
@@ -2,6 +2,12 @@ import fastapi
 
 app = fastapi.FastAPI()
 
+menu = [{   'id': 1,        'name': 'coffee',      'price': 2.5     },
+        {   'id': 2,        'name': 'cake',        'price': 10    },
+        {   'id': 3,        'name': 'tea',         'price': 3.2    },
+        {   'id': 4,        'name': 'croissant',   'price': 5.79    }]
+
+
 @app.get("/")
 def indice():
     return{"mensagem" : "Ola Mundo!"}
