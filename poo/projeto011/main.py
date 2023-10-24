e7cd1fa588c440436476b5fe81c9fdfffa14d20b
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2023-10-24 22:13:34 +0000

    Tue, Oct 24, 2023, 7:13 PM -03:00

--- poo/projeto011/main.py
+++ poo/projeto011/main.py
@@ -6,8 +6,13 @@ menu = [{   'id': 1,        'name': 'coffee',      'price': 2.5     },
         {   'id': 2,        'name': 'cake',        'price': 10    },
         {   'id': 3,        'name': 'tea',         'price': 3.2    },
         {   'id': 4,        'name': 'croissant',   'price': 5.79    }]
-
-
 @app.get("/")
 def indice():
     return{"mensagem" : "Ola Mundo!"}
+
+@app.get('/get-item/{item_id}')
+def get_item(item_id: int = fastapi.Path(...,description="Preencha com o ID do item que deseja consultar")):
+    search = list(filter(lambda x: x["id"] == item_id, menu))
+    if search == []:
+        return {'Error': 'Item does not exist'}
+    return {'Item': search[0]}
