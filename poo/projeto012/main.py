1e674c500e81662f394a0221d75d254c9cfc07fd
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2023-10-27 23:35:47 +0000

    Fri, Oct 27, 2023, 8:35 PM -03:00

--- poo/projeto012/main.py
+++ poo/projeto012/main.py
@@ -14,3 +14,13 @@ def getContinents():
         Continents_list.append( data_continents )
     mycursor.close()
     return {"Continents": Continents_list}
+
+@app.get("/getContinent/{continent_id}")
+def getContinent(continent_id : int):
+    Continents_list = []
+    mycursor = connection.mydb.cursor(dictionary=True)
+    mycursor.execute("select * from continents where continent_id = {0}".format(continent_id))
+    for data_continents in mycursor:
+        Continents_list.append( data_continents )
+    mycursor.close()
+    return {"Continent": Continents_list}
