38fa4fe46050cec8d6256899b1ec0efd0eb88b3d
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2023-10-27 23:26:06 +0000

    Fri, Oct 27, 2023, 8:26 PM -03:00

--- poo/projeto012/main.py
+++ poo/projeto012/main.py
@@ -4,3 +4,13 @@ app = fastapi.FastAPI()
 @app.get("/")
 def hello_world_root():
     return {"Olá": "Funciona"}
+
+@app.get("/getContinents")
+def getContinents():
+    Continents_list = []
+    mycursor = connection.mydb.cursor(dictionary=True)
+    mycursor.execute("select * from continents")
+    for data_continents in mycursor:
+        Continents_list.append( data_continents )
+    mycursor.close()
+    return {"Continents": Continents_list}
