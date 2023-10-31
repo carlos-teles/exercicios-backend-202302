0cab11ff014a31065bfb091091206445634b53da
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2023-10-31 21:41:48 +0000

    Tue, Oct 31, 2023, 6:41 PM -03:00

--- poo/projeto012/app.py
+++ poo/projeto012/app.py
@@ -1,9 +1,17 @@
 import flask
+import requests
 app = flask.Flask(__name__)
 
 @app.route("/")
 def OlaMundo():
         return "<p>Ola Mundo</p>"
+
+@app.route("/consultaContinente")
+def consultaContinente():
+    import requests
+    response2 = requests.get("https://www.omdbapi.com/?apikey=XXX&t=dune")
+    return response2.json()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
