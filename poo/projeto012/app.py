9b83dc7600d48d869b91dd3d12790ec7dca2fb8e
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2023-10-31 21:52:22 +0000

    Tue, Oct 31, 2023, 6:52 PM -03:00

--- poo/projeto012/app.py
+++ poo/projeto012/app.py
@@ -1,6 +1,6 @@
 import flask
 import requests
-import 
+from json2table import convert
 app = flask.Flask(__name__)
 
 @app.route("/")
@@ -9,8 +9,15 @@ def OlaMundo():
 
 @app.route("/consultaContinentes")
 def consultaContinentes():
-    response2 = requests.get("http://127.0.0.1:8000/getContinents")
-    return response2.json()
+        response2 = requests.get("http://127.0.0.1:8000/getContinents")
+
+        json_object = response2.json()
+        build_direction = "LEFT_TO_RIGHT"
+        table_attributes = {"style" : "width:100%"}
+        html = convert(json_object, build_direction=build_direction, table_attributes=table_attributes)
+        #print(html)
+        #return response2.json()
+        return html
 
 
 if __name__ == '__main__':
