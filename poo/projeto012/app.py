db6b1978e6b0fe7ee2a658f40535b4cd2d84e745
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2023-11-03 12:02:18 +0000

    Fri, Nov 3, 2023, 9:02 AM -03:00

--- poo/projeto012/app.py
+++ poo/projeto012/app.py
@@ -50,7 +50,8 @@ def consultaRegioes():
 
 @app.route("/getRegion")
 def getRegion():
-        response2 = requests.get("http://127.0.0.1:8000/getRegion")
+        continent_id = request.args.get('continent_id')
+        response2 = requests.get("http://127.0.0.1:8000/getRegion/"+continent_id)
         json_object = response2.json()
         build_direction = "LEFT_TO_RIGHT"
         table_attributes = {"style" : "width:100%", "border": "1px solid black"}
