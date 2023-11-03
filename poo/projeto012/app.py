3fbbf5b749a25b4f00746aaf78a62073f3c19587
Carlos A M S Teles <carlos.teles@eic.cefet-rj.br>
2023-11-03 02:41:53 +0000

    Thu, Nov 2, 2023, 11:41 PM -03:00

--- poo/projeto012/app.py
+++ poo/projeto012/app.py
@@ -33,7 +33,28 @@ def consultaContinentes():
 @app.route("/consultaRegioes")
 def consultaRegioes():
         response2 = requests.get("http://127.0.0.1:8000/getRegions")
+        json_object = response2.json()
+        build_direction = "LEFT_TO_RIGHT"
+        table_attributes = {"style" : "width:100%", "border": "1px solid black"}
+        html = json2table.convert(json_object, build_direction=build_direction, table_attributes=table_attributes)
+        #print(html)
+        #return response2.json()
+        return html
 
+@app.route("/consultaPaises")
+def consultaPaises():
+        response2 = requests.get("http://127.0.0.1:8000/getCountries")
+        json_object = response2.json()
+        build_direction = "LEFT_TO_RIGHT"
+        table_attributes = {"style" : "width:100%", "border": "1px solid black"}
+        html = json2table.convert(json_object, build_direction=build_direction, table_attributes=table_attributes)
+        #print(html)
+        #return response2.json()
+        return html
+
+@app.route("/consultaLinguas")
+def consultaLinguas():
+        response2 = requests.get("http://127.0.0.1:8000/getCountryLanguages")
         json_object = response2.json()
         build_direction = "LEFT_TO_RIGHT"
         table_attributes = {"style" : "width:100%", "border": "1px solid black"}
